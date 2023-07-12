0a21e0cc1b166b67af775d2694abfe92cb97b8a1
F0xyBG <minsko@abv.bg>
2023-07-12 14:33:18 +0300

    showcase database upload

--- BackEnd/petfinder.sql
+++ BackEnd/petfinder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Време на генериране: 11 юли 2023 в 14:26
+-- Време на генериране: 12 юли 2023 в 13:32
 -- Версия на сървъра: 10.4.27-MariaDB
 -- Версия на PHP: 8.2.0
 
@@ -55,6 +55,15 @@ CREATE TABLE `posts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Схема на данните от таблица `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `text`, `images`, `phone`, `name`, `category`, `status`, `location`, `date`) VALUES
+(1, 'Кучето ми Пепи избяга! Моля помогнете!', 'Кучето ми Пепи избяга от вкъщи и отиде в посока 27 блок на квартал Тева', 'http://localhost:3000/images/image-1689078937755.webp', '08969696969', 'Георги', 'Куче', 'searching_for_pet', 'Перник', '2023-07-11 15:35:37'),
+(2, 'Котето ми просто си излезе!', 'Котето ми Гошко просто излезе от вкъщи и не се върна. Моля ако го видите в областта на кв. Мошино да ми се обадите незабавно!', 'http://localhost:3000/images/image-1689079062215.jpg', '08969696969', 'Петър', 'Котка', 'searching_for_pet', 'Перник', '2023-07-11 15:37:42'),
+(3, 'Загубих си котето ', 'Моля ви помогнете ми да си взема мацките от Тева!', 'http://localhost:3000/images/image-1689082368438.jpg', '0888987876', 'Денислав Димчев', 'Котка', 'searching_for_pet', 'Перник', '2023-07-11 16:32:48');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -85,7 +94,7 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Ограничения за дъмпнати таблици
